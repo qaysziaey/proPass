4f693505af7efe0b594348bb904dafd0ee8dc5a2
qaysziaey <techcorner95@gmail.com>
2024-04-13 16:07:06 +0200

    add variable to .env.local

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -5,19 +5,22 @@ import { Button, Typography } from "@material-tailwind/react";
 
 export default function NotFoundPage() {
   return (
-    <div className="not-found-page-bg flex min-h-[100vh] flex-col items-center justify-center space-y-4 px-4 pb-12 text-center md:px-6">
+    <div className="not-found-page-bg flex min-h-[100vh] flex-col items-center justify-center space-y-4 bg-black px-4 pb-12 text-center md:px-6">
       <div className="flex flex-col items-center gap-4 space-y-4">
-        <h1 className="text-4xl font-bold tracking-tighter text-gray-200 sm:text-5xl/none md:text-6xl/none">
-          404
-        </h1>
         <Typography
           variant="h1"
-          className="to-gray max-w-[600px] bg-gradient-to-r from-gray-300 bg-clip-text py-4 text-3xl font-bold tracking-tighter text-transparent md:text-7xl/relaxed lg:text-8xl/relaxed xl:text-9xl/relaxed"
+          className="tracking-loose to-gray max-w-[600px] bg-gradient-to-b from-red-800  to-black bg-clip-text py-4  font-bold text-transparent blur-sm md:text-4xl/relaxed  lg:text-5xl/relaxed xl:text-9xl/relaxed"
+        >
+          404
+        </Typography>
+        <Typography
+          variant="h1"
+          className="to-gray max-w-[600px] bg-gradient-to-r from-gray-400 bg-clip-text  font-bold tracking-tighter text-transparent  md:text-4xl/relaxed lg:text-5xl/relaxed xl:text-6xl/relaxed"
         >
           This page does not exist.
         </Typography>
         <div className="flex flex-col items-center gap-4">
-          <p className="max-w-[600px] text-gray-500 dark:text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+          <p className="my-0 max-w-[600px] py-0 text-gray-500 dark:text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
             Sorry, we couldn&apos;t find the page you were looking for.
           </p>
           <Link href="/">
@@ -25,9 +28,11 @@ export default function NotFoundPage() {
               size="md"
               color="white"
               variant="text"
-              className="text-small flex items-center gap-2 rounded-lg px-4 py-3 font-medium normal-case transition-all  hover:bg-white "
+              className="text-small flex items-center gap-2 rounded-lg px-4 py-3 font-medium normal-case text-gray-500 transition-all  hover:bg-gray-900 hover:text-white "
             >
-              Back to Home
+              <Typography variant="small" className="font-bold tracking-wide ">
+                Go Home
+              </Typography>
             </Button>
           </Link>
         </div>
--- src/lib/mongo/connectDB.js
+++ src/lib/mongo/connectDB.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
   throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
+    "Please define the MONGODB_URI environment variable inside .env.local",
   );
 }
 
